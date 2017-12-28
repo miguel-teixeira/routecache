df22222aef81a9c9c510a033b48ee03a06a96c52
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-12-28 17:44:53 +0000

    add to git

---
+++ src/RouteCache.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Cache;
+
+class CachedRoute
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $lifetime)
+    {
+        $cacheKey = $this->getCacheKey($request);
+
+        if (Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
+        $response = $next($request);
+
+        Cache::put($cacheKey, $response, $lifetime/60);
+
+        return $response;
+    }
+
+    protected function getCacheKey($request)
+    {
+        return $request->getMethod() . '/' .
+            $request->path() . '/' .
+            md5(json_encode($request->all()));
+    }
+}
